c0b8c60173da1bfae073be6b7f2cf52ad7ea0d33
kajela <titoyut56@gmail.com>
2022-06-07 01:47:56 -0400

    add the initial files

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,30 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int unit = 10;
+        switch (unit) {
+            case 1:
+                System.out.println("blockchain technologies");
+                break;
+            case 2:
+                System.out.println("knowledge management");
+                break;
+            case 3:
+                System.out.println("probability and stastics");
+                break;
+            case 4:
+                System.out.println("java programming");
+            case 5:
+                System.out.println("computer networks");
+                break;
+            case 6:
+                System.out.println("web design");
+                break;
+            default:
+                System.out.println("looking forwad to next semister");
+        }
+
+      //  System.out.println("Hello world!");
     }
 }
